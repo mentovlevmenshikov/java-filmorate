8e0d6cc8e9d95b66f4123660799886ac665488a0
Alexey Menshikov <mentovlevmenshikov@ya.ru>
2024-06-17 01:32:36 +0500

    Fix check style

--- src/main/java/ru/yandex/practicum/filmorate/config/JacksonConfiguration.java
+++ src/main/java/ru/yandex/practicum/filmorate/config/JacksonConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.format.DateTimeFormatter;
+
 @Configuration
 public class JacksonConfiguration {
 
