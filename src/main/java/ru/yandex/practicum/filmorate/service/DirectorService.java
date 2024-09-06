ed51130098670c3a2edc96becb60ce51c210cd22
iamdnikolaev <160739477+iamdnikolaev@users.noreply.github.com>
2024-09-06 15:36:56 +0300

    Спринт 13-групповой. Добавлен функционал режиссеров. Теперь с удалением.

--- src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
@@ -2,11 +2,19 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.repository.DeleteStorage;
 import ru.yandex.practicum.filmorate.repository.ModelRepository;
 
 @Service
 public class DirectorService extends ModelService<Director> {
+    private final DeleteStorage deleteStorage;
+
     public DirectorService(ModelRepository<Director> modelRepository) {
         super(modelRepository);
+        deleteStorage = (DeleteStorage) modelRepository;
+    }
+
+    public void delete(long id) {
+        deleteStorage.delete(id);
     }
 }
